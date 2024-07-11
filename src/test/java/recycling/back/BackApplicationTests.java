f9c15b2be6134a280d5481469ca05e3c7367e663
jinhyeokko <jinhyeok.ko@outlook.com>
2024-07-12 05:34:43 +0900

    spring boot

---
+++ src/test/java/recycling/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package recycling.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
