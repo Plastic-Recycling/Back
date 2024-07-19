a46983453da1ce86222da68e971b351ade6df59d
jinhyeokko <jinhyeok.ko@outlook.com>
2024-07-19 16:43:26 +0900

    ## Register * 단순히 이메일을 입력 후 인증 링크만 보내던 api를 인증이 한 번 진행된 이메일이면 토큰을 요구하도록 변경 * 이메일 인증 후 단순 이메일만 캐시에 저장해두던 것을 시크릿 코드를 생성해 value를 저장   * 가입 폼을 작성하던 도중 브라우저를 종료할 경우, 해당 시크릿 코드를 통해 진행 가능

--- src/main/java/recycling/back/user/register/dto/RegisterUser.java
+++ src/main/java/recycling/back/user/register/dto/RegisterUser.java
@@ -9,6 +9,16 @@ public class RegisterUser {
 
     private String password;
 
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
     public String getEmail() {
         return email;
     }
--- src/main/java/recycling/back/util/ResponseUtil.java
+++ src/main/java/recycling/back/util/ResponseUtil.java
@@ -5,11 +5,23 @@ import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
 import java.net.URI;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ResponseUtil {
     public static ResponseEntity<String> ok(String message) {
         return ResponseEntity.status(HttpStatus.OK).body(message);
+    }
+
+    public static ResponseEntity<Map<String, Object>> ok(boolean a, String message){
+        Map<String, Object> response = new HashMap<>();
+        response.put("isNewEmail", a);
+        response.put("message", message);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    public static ResponseEntity<String> bad(String message){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
     }
 
     public static ResponseEntity<String> found(String url) {
@@ -19,5 +31,7 @@ public class ResponseUtil {
     public static ResponseEntity<Map<String, BigDecimal>> ok(Map<String, BigDecimal> rate){
         return ResponseEntity.status(HttpStatus.OK).body(rate);
     }
+
+    //public static ResponseEntity<Object>
 
 }
